a0258cd8983dab3acd1a71eda0fb65ddc4b17da3
Joe Peck <jpeckjrseau@icloud.com>
2018-07-23 23:45:41 -0500

    updates

---
+++ app/data/friends.js
@@ -0,0 +1,52 @@
+let friendsArray = [
+    {
+      name: "William James",
+      photo: "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9c/William_James_b1842c.jpg/220px-William_James_b1842c.jpg",
+      scores: [
+          3,
+          3,
+          4,
+          5,
+          1,
+          2,
+          3,
+          4,
+          5,
+          1,
+      ]
+     },
+    {
+      name: "Ludwig Wittgenstein",
+      photo: "https://upload.wikimedia.org/wikipedia/commons/thumb/6/60/35._Portrait_of_Wittgenstein.jpg/220px-35._Portrait_of_Wittgenstein.jpg",
+      scores: [
+          5,
+          5,
+          5,
+          3,
+          2,
+          1,
+          3,
+          5,
+          1,
+          2,
+      ]
+    },
+    {
+      name: "Jacques Dérrida",
+      photo: "https://upload.wikimedia.org/wikipedia/en/4/44/Derrida_main.jpg",
+      scores: [
+          5,
+          5,
+          5,
+          2,
+          4,
+          2,
+          3,
+          5,
+          2,
+          2,
+        ]
+     }
+  ];
+
+  module.exports = friendsArray;
---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,53 @@
+var path = require("path");
+var friends = require("../data/friends.js");
+
+module.exports = function(app) {
+  app.get("/api/friends", function(req, res) {
+    res.json(friends);
+  });
+
+  app.post("/api/friends", function(req, res) {
+    console.log("got to post route");
+    var array = friends.scores;
+    var friendsCount = 0;
+    var bestMatch =0; 
+    var scoresArray =[];
+    var userInput = req.body;
+
+    console.log("this is the req.body: " + req.body);
+    var userresponse = userInput.scores;
+    // var me = JSON.stringify(userInput);
+    console.log(userInput.scores);
+    console.log(userresponse);
+    // console.log(typeof(req.body.score[0]));
+
+    console.log("this is the userresponse: " + userresponse);
+
+    var matchName="";
+    var matchImage="";
+    for (var i =0 ; i <friends.length; i++){
+        var diff = 0;
+
+        for (var j=0; j<userresponse.length; j++){
+            diff += Math.abs(parseInt(friends[i].scores[j]- userresponse[j]))
+        }
+       scoresArray.push(diff);
+    }
+
+    for(var i =0 ; i<scoresArray.length; i++){
+        if(scoresArray[i]< scoresArray[bestMatch]){
+        bestMatch = i
+        }
+    }
+    
+    var bestfriend = friends[bestMatch];
+    console.log("b",bestfriend);
+    console.log("c",bestMatch);
+
+    res.json(bestfriend);
+    console.log(bestfriend);
+
+    console.log(typeof(friends));
+    // friends.push(req.body);
+  });
+};
