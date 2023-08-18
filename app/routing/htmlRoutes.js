c38b468c37068fa67a90256f07eb5df929f8a77c
Joseph B Peck Jr <jpeckjrseau@icloud.com>
2023-08-17 20:16:02 -0500

    moved survey html

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -2,14 +2,14 @@ var path = require("path");
 
 module.exports = function(app) {
     app.get("/survey", function(req, res) {
-      res.sendFile(path.join(__dirname, "../public/survey.html"));
+      res.sendFile(path.join(__dirname, "../survey.html"));
     });
   
     app.get("*", function(req, res) {
-      res.sendFile(path.join(__dirname, "../public/home.html"));
+      res.sendFile(path.join(__dirname, "../index.html"));
     });
     app.get("/", function(req, res) {
-      res.sendFile(path.join(__dirname, "../public/home.html"));
+      res.sendFile(path.join(__dirname, "../index.html"));
     });
 
   }
